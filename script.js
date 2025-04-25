1a4cd98fcaa1a0b48c5771fb95346228cd341f0f
Maria Laura Castro <cmarialaura599@gmail.com>
2025-04-25 15:17:22 -0300

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+function adicionarTarefa() {
+    var msgSucesso = 'Tarefa adicionada com sucesso!'
+    var msgErro = 'Escreva uma tarefa válida!' 
+
+    var inputTarefa = document.getElementById('inputTarefa')
+    var tarefa = inputTarefa.value
+
+    if (tarefa == " ") {
+        document.getElementById('mensagemErro').textContent = msgErro
+        document.getElementById('mensagemSucesso').textContent = " "
+    } else {
+        document.getElementById('mensagemSucesso').textContent = msgSucesso
+        document.getElementById('mensagemErro').textContent = " " 
+
+        var lista = document.getElementById('listaTarefas')
+        var novaTarefa = document.createElement('li')
+    
+        novaTarefa.textContent = tarefa
+        lista.appendChild(novaTarefa)
+    
+        inputTarefa.value = ""
+    }
+
+}
